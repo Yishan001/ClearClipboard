b279e46d0cf246b542c4b583e71cf860a3d7aca9
yishan <maoyishan@gmail.com>
2023-02-01 11:55:30 +0800

    first commit.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClearClipboard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            InitializeComponent();
+
+            Clipboard.Clear();
+
+            SetTimer();
+        }
+
+        private static void SetTimer()
+        {
+            System.Timers.Timer aTimer = new System.Timers.Timer(500);
+            aTimer.Elapsed += OnTimerEvent;
+            aTimer.Start();
+        }
+
+        private static void OnTimerEvent(Object? source, System.Timers.ElapsedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
